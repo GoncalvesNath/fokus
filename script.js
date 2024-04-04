f2fa255d463a5c9d659c3283f31c11285514c90b
Nathalia Dias <158405216+ndiasg@users.noreply.github.com>
2024-04-03 23:24:17 -0300

    ajustando start e pause do temporizador

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ const botoes = document.querySelectorAll('.app__card-button');
 const startPauseBt = document.querySelector('#start-pause')
 const musicaFocoInput = document.querySelector('#alternar-musica');
 const musica = new Audio('/sons/luna-rise-part-one.mp3') /*poderiamos usar o readfile mas ele pode atrasar o caregamento do projeto, por isso o uso de variáveis no inico é mais funcional */
+const audioPlay = new Audio('/sons/play.wav');
+const audioPausa = new Audio('/sons/pause.mp3');
+const audioTempoFinalizado = new Audio('./sons/beep.mp3')
 
 let tempoDecorridoEmSegundos = 5;
 let intervaloId = null
@@ -16,7 +19,7 @@ musica.loop = true
 
 
 musicaFocoInput.addEventListener('change', () => {
-    if(musica.paused) {
+    if (musica.paused) {
         musica.play()
     } else {
         musica.pause()
@@ -41,7 +44,7 @@ longoBt.addEventListener('click', () => {
 });
 
 function alterarContexto(contexto) {
-    botoes.forEach(function (contexto){
+    botoes.forEach(function (contexto) {
         contexto.classList.remove('active')
     })
     html.setAttribute('data-contexto', contexto)
@@ -69,12 +72,29 @@ function alterarContexto(contexto) {
 }
 
 const contagemRegressiva = () => {
-    //iniciar()
+    if (tempoDecorridoEmSegundos <= 0) {
+        audioTempoFinalizado.play()
+        zerar()
+        alert('Tempo finalizado!')
+        return
+    }
     tempoDecorridoEmSegundos -= 1
     console.log('Temporizador: ' + tempoDecorridoEmSegundos)
 }
 
-startPauseBt.addEventListener('click', contagemRegressiva)
-function iniciar() {
+startPauseBt.addEventListener('click', iniciarouPausar)
+
+function iniciarouPausar() {
+    if (intervaloId) {
+        audioPausa.play()
+        zerar()
+        return
+    }
+    audioPlay.play()
     intervaloId = setInterval(contagemRegressiva, 1000) /*1000 determina 1 segundo pois a contagem é feita em milisegundos*/
+}
+
+function zerar() {
+    clearInterval(intervaloId)
+    intervaloId = null
 }
